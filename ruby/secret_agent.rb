13cf9376c1debd58835f2a971f640fca09d2831c
Jamie Davella <jamie.davella@gmail.com>
2016-04-17 18:34:06 -0400

    added to secret_agent.rb code for driver input and added pseudo code for methods

--- ruby/secret_agent.rb
+++ ruby/secret_agent.rb
@@ -16,25 +16,58 @@
 # =>       -then access the letter right before it by using subtract operation and storing in variable
 # => -print results
 
+# OUR METHOD DECLARATIONS
+
+#encrypts a word by moving each letter forward 1 letter
 def encrypt(str)
 	index = 0
+	word = ""
 	while index < str.length
-		puts str[index].next
+		word += str[index].next
 		index += 1
 	end
+	puts word
 end
 
-encrypt("zed")
+#encrypt("swordfish")
 
+#this method decrypts a string by moving each letter back a letter
 def decrypt(str)
 	index = 0
 	alphabet = "abcdefghijklmnopqrstuvwxyz"
-	
+	word = ""
 	while index < str.length
 		decrypt_letter = alphabet.index(str[index])
-		puts alphabet[decrypt_letter - 1]
+		word += alphabet[decrypt_letter - 1]
 		index += 1
 	end
+	puts word
 end
 
-decrypt("afe")
+#decrypt("zed")
+
+#decrypt(encrypt("swordfish"))
+#does not work
+
+#ask user whether they want to decrypt or encrypt a password using puts and gets.chomp
+#ask for password using puts and store in variable in gets.chomp
+#create loop
+
+# OUR DRIVER CODE 
+
+# ask user whether want encryption or decryption
+puts "Would you like to encrypt or decrypt a password (encrypt/decrypt)?"
+encryption_type = gets.chomp.downcase
+
+# asks user for a password
+puts "What password do you want to use?"
+password = gets.chomp.downcase
+
+# runs methods to encrypt or decrypt a password.
+if encryption_type == "encrypt"
+	encrypt("#{password}")
+else encryption_type == "decrypt"
+	decrypt("#{password}")
+end
+
+
