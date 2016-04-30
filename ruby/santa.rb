45564d0b8fb67d9fe3e053803ee952dc64d20131
Jamie Davella <jamie.davella@gmail.com>
2016-04-30 14:52:44 -0400

    finished release 1 by adding driver code to add initializations and tested with methods on each new initialized santa in the array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,8 +1,12 @@
 # Declare Santa Class and give it three instance methods
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa Instance"
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 
 	def speak
@@ -15,7 +19,29 @@ class Santa
 end
 
 # Initialize a Santa instance and call its methods
-jamie = Santa.new
-jamie.speak
-jamie.eat_milk_and_cookies("chocolate chip")
+# jamie = Santa.new
+# jamie.speak
+# jamie.eat_milk_and_cookies("chocolate chip")
 
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+list_genders = ["female", "male", "transgender", "unknown"]
+list_ethnicities = ["Italian", "French", "Peruvian", "Troll"]
+
+list_genders.length.times do |i|
+	santas << Santa.new(list_genders[i], list_ethnicities[i])
+end
+
+santas.each do |santa|
+	santa.eat_milk_and_cookies("chocolate chip")
+	santa.speak
+end
+
+p santas
