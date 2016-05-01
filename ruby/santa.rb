81060a8e67024678752e91146c9bf7b3abdb5578
Jamie Davella <jamie.davella@gmail.com>
2016-05-01 00:07:49 -0400

    added attribute changing methods, getter and setter methods, and driver code to finish release 2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,13 +16,52 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie} cookie"
 	end
+	
+	#add three attribute changing methods to Santa Class
+	def celebrate_birthday
+		@age = @age + 1
+	end
+
+	def get_mad_at(name)
+		index = @reindeer_ranking.index(name)
+		@reindeer_ranking = @reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(index))
+		p @reindeer_ranking
+	end
+
+	#setter method to change @gender from outside the class definition
+	def change_gender=(new_gender)
+		@gender = new_gender
+		# p @gender
+	end
+
+	#add two getter methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
+	end
 end
 
-# Initialize a Santa instance and call its methods
-# jamie = Santa.new
-# jamie.speak
-# jamie.eat_milk_and_cookies("chocolate chip")
+#Initialize a Santa instance and call its methods
+# jamie = Santa.new("Female", "Italian and French")
+# jamie.get_mad_at("Rudolph")
+# jamie.celebrate_birthday
+# # using age getter in the below puts statement because if I run the celebrate_birthaday method
+# # again it will increase the age by one again and I don't want to change age again.
+# puts "this Santa is now #{jamie.age} years old"
+#  jamie.change_gender = "Male"
+#  # will need to add a getter for gender in order to use this new gender assignment in code
+#  # outside the class, or I can put in the method with parameters to use it in outside code.
+#  #puts "#{jamie.change_gender = "male"} is the new gender"
+# jamie.age
+# puts "Again, this Santa's age is #{jamie.age}"
+# jamie.ethnicity
+# puts "this Santa's ethnicity is #{jamie.ethnicity}"
+# p jamie
 
+#initialize more Santas with specific gender and ethnicity characteristics.
 santas = []
 santas << Santa.new("agender", "black")
 santas << Santa.new("female", "Latino")
@@ -44,4 +83,6 @@ santas.each do |santa|
 	santa.speak
 end
 
-p santas
+santas.each do |santa|
+	puts "this Santa's ethnicity is #{santa.ethnicity}"
+end
