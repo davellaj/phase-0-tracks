5045d52c314ade81db16397d9466174e517f7aae
Jamie Davella <jamie.davella@gmail.com>
2016-05-01 00:37:46 -0400

    finished release 3 changing code to use attr_reader and attr_accessor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,7 @@
 # Declare Santa Class and give it three instance methods
 class Santa
+	attr_reader :age, :reindeer_ranking
+	attr_accessor :gender, :ethnicity
 
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa Instance"
@@ -28,61 +30,61 @@ class Santa
 		p @reindeer_ranking
 	end
 
-	#setter method to change @gender from outside the class definition
-	def change_gender=(new_gender)
-		@gender = new_gender
-		# p @gender
-	end
+	# #setter method to change @gender from outside the class definition
+	# def gender=(new_gender)
+	# 	@gender = new_gender
+	# 	# p @gender
+	# end
 
-	#add two getter methods
-	def age
-		@age
-	end
+	# #add two getter methods
+	# def age
+	# 	@age
+	# end
 
-	def ethnicity
-		@ethnicity
-	end
+	# def ethnicity
+	# 	@ethnicity
+	# end
 end
 
 #Initialize a Santa instance and call its methods
-# jamie = Santa.new("Female", "Italian and French")
-# jamie.get_mad_at("Rudolph")
-# jamie.celebrate_birthday
-# # using age getter in the below puts statement because if I run the celebrate_birthaday method
-# # again it will increase the age by one again and I don't want to change age again.
-# puts "this Santa is now #{jamie.age} years old"
-#  jamie.change_gender = "Male"
-#  # will need to add a getter for gender in order to use this new gender assignment in code
-#  # outside the class, or I can put in the method with parameters to use it in outside code.
-#  #puts "#{jamie.change_gender = "male"} is the new gender"
-# jamie.age
-# puts "Again, this Santa's age is #{jamie.age}"
-# jamie.ethnicity
-# puts "this Santa's ethnicity is #{jamie.ethnicity}"
-# p jamie
+jamie = Santa.new("Female", "Italian and French")
+jamie.get_mad_at("Rudolph")
+jamie.celebrate_birthday
+# using age getter in the below puts statement because if I run the celebrate_birthaday method
+# again it will increase the age by one again and I don't want to change age again.
+puts "this Santa is now #{jamie.age} years old"
+ jamie.gender = "Male"
+ # will need to add a getter for gender in order to use this new gender assignment in code
+ # outside the class, or I can put in the method with parameters to use it in outside code.
+ puts "#{jamie.gender} is the new gender"
+jamie.age
+puts "Again, this Santa's age is #{jamie.age}"
+jamie.ethnicity
+puts "this Santa's ethnicity is #{jamie.ethnicity}"
+p jamie
 
-#initialize more Santas with specific gender and ethnicity characteristics.
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+# #initialize more Santas with specific gender and ethnicity characteristics.
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
 
-list_genders = ["female", "male", "transgender", "unknown"]
-list_ethnicities = ["Italian", "French", "Peruvian", "Troll"]
+# list_genders = ["female", "male", "transgender", "unknown"]
+# list_ethnicities = ["Italian", "French", "Peruvian", "Troll"]
 
-list_genders.length.times do |i|
-	santas << Santa.new(list_genders[i], list_ethnicities[i])
-end
+# list_genders.length.times do |i|
+# 	santas << Santa.new(list_genders[i], list_ethnicities[i])
+# end
 
-santas.each do |santa|
-	santa.eat_milk_and_cookies("chocolate chip")
-	santa.speak
-end
+# santas.each do |santa|
+# 	santa.eat_milk_and_cookies("chocolate chip")
+# 	santa.speak
+# end
 
-santas.each do |santa|
-	puts "this Santa's ethnicity is #{santa.ethnicity}"
-end
+# santas.each do |santa|
+# 	puts "this Santa's ethnicity is #{santa.ethnicity}"
+# end
