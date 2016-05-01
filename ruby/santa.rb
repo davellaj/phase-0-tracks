139960ea77a7aea52de471acfe901762eae679d7
Jamie Davella <jamie.davella@gmail.com>
2016-05-01 02:03:13 -0400

    finished release 4 by initializing many santas and changing their age and printing attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,8 @@
 # Declare Santa Class and give it three instance methods
 class Santa
-	attr_reader :age, :reindeer_ranking
-	attr_accessor :gender, :ethnicity
+	# add getters and setters to class to access attributes outside class
+	attr_reader :reindeer_ranking
+	attr_accessor :gender, :ethnicity, :age
 
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa Instance"
@@ -46,24 +47,26 @@ class Santa
 	# end
 end
 
-#Initialize a Santa instance and call its methods
-jamie = Santa.new("Female", "Italian and French")
-jamie.get_mad_at("Rudolph")
-jamie.celebrate_birthday
-# using age getter in the below puts statement because if I run the celebrate_birthaday method
-# again it will increase the age by one again and I don't want to change age again.
-puts "this Santa is now #{jamie.age} years old"
- jamie.gender = "Male"
- # will need to add a getter for gender in order to use this new gender assignment in code
- # outside the class, or I can put in the method with parameters to use it in outside code.
- puts "#{jamie.gender} is the new gender"
-jamie.age
-puts "Again, this Santa's age is #{jamie.age}"
-jamie.ethnicity
-puts "this Santa's ethnicity is #{jamie.ethnicity}"
-p jamie
+# #Initialize a Santa instance and call its methods and use its attributes
+# jamie = Santa.new("Female", "Italian and French")
+# jamie.get_mad_at("Rudolph")
+# jamie.celebrate_birthday
+# # using age getter in the below puts statement because if I run the celebrate_birthaday method
+# # again it will increase the age by one again and I don't want to change age again.
+# puts "this Santa is now #{jamie.age} years old"
+# jamie.gender = "Male"
+# # will need to add a getter for gender in order to use this new gender assignment in code
+# # outside the class, or I can put in the method with parameters to use it in outside code.
+# puts "#{jamie.gender} is the new gender"
+# jamie.age
+# puts "Again, this Santa's age is #{jamie.age}"
+# jamie.ethnicity
+# puts "this Santa's ethnicity is #{jamie.ethnicity}"
+# jamie.ethnicity = "Dominican"
+# puts "this Santa's ethnicity is #{jamie.ethnicity}"
+# p jamie
 
-# #initialize more Santas with specific gender and ethnicity characteristics.
+# # initialize more Santas with specific gender and ethnicity characteristics.
 # santas = []
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
@@ -73,6 +76,7 @@ p jamie
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
 
+# # use a shorter way of adding santas to the array
 # list_genders = ["female", "male", "transgender", "unknown"]
 # list_ethnicities = ["Italian", "French", "Peruvian", "Troll"]
 
@@ -80,6 +84,7 @@ p jamie
 # 	santas << Santa.new(list_genders[i], list_ethnicities[i])
 # end
 
+# # Use instance method to perform task for each santa in the array
 # santas.each do |santa|
 # 	santa.eat_milk_and_cookies("chocolate chip")
 # 	santa.speak
@@ -87,4 +92,24 @@ p jamie
 
 # santas.each do |santa|
 # 	puts "this Santa's ethnicity is #{santa.ethnicity}"
+# end
+
+# release 4 create many santas
+# # use a shorter way of adding santas to the array
+list_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "transgender", "unknown"]
+list_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Italian", "French", "Peruvian", "Troll"]
+
+
+#initialize many santas and thenn change the age of each santa to a random age and print attributes for that santa
+50.times do
+santa = Santa.new(list_genders.sample, list_ethnicities.sample)
+santa.age = rand(0..140)
+p santa
+end
+
+# roundabout way of choosing random item from array list
+# 50.times do |i|
+# santa = Santa.new(list_genders[rand(list_genders.length)], list_ethnicities[rand(list_ethnicities.length)])
+# # age = rand()
+# p santa
 # end
