26fbbcd0716019ec73b96a7542e7f04eea7c821d
Jamie Davella <jamie.davella@gmail.com>
2016-05-02 01:13:22 -0400

    added methods for Class to complete release 1

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -19,15 +19,47 @@
 	# Change preference, Add fav Broadway show
 	# change preference, Delete fav broadway show
 
-Class Broadway_star
-attr_reader = :experience, :age
-attr_accessor = :voice_type, :name, :fav_shows
+class Broadway_star
+attr_reader :experience, :age
+attr_accessor :voice_type, :name, :fav_shows
 
 	def initialize(name, age, voice_type)
+		puts "Initializing new instance"
 		@name = name
 		@age = age
 		@voice_type = voice_type
 		@experience = 0
 		@fav_shows = ["Beautiful", "Les Miserables", "Book of Mormon", "Mama Mia"]
 	end
+
+	def sing_loudly(sing)
+		sing.upcase
+	end
+
+	def gain_experience
+		@experience += 1
+	end
+
+	def add_fav_shows(show)
+		@fav_shows << show
+	end
+
+	def delete_fav_shows(show)
+		@fav_shows.delete(show)
+	end
 end
+
+#test code
+performer = Broadway_star.new("jamie", 28, "soprano")
+# p performer.sing_loudly("My heart will go on")
+# p performer.gain_experience
+# p performer
+# p performer.age
+# puts "#{performer.name} has #{performer.experience} years of experience"
+# performer.gain_experience
+# p performer.experience 
+# p performer.fav_shows
+# performer.delete_fav_shows("Beautiful")
+# p performer.fav_shows
+# fav_shows = ["the color purple", "lion king"]
+# p fav_shows
