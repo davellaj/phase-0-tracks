7ae16685a92fbf4ac8df187a361efb12b74e5595
Jamie Davella <jamie.davella@gmail.com>
2016-05-02 00:07:26 -0400

    pseudocode new Class for Broadway Performer

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+# Create a class for Broadway Performer
+# set attr_reader 
+# set attr_accessor 
+
+# Initialize with attributes (have 3 parameters)
+# Attributes will be:
+	# - years of experience - everyone starts with 0
+	# - Broadway show favorites ["Beautiful", "Les Miserables", "Book of Mormon", "Mama Mia"]
+	# - age
+		# this will vary
+	# - name
+		# this will vary
+	# - voice type (Tenor, Soprano, Contralto, Baritone, Bass) 
+		# this will vary
+
+#Add 3 Methods 
+	# Sing loudly(that takes an argument of words and capitalize them)
+	# Gain experience - adds 1 to experienceand reassigns value or experience
+	# Change preferences of Broadway shows
