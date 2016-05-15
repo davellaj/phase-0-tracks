999459bdffaf65903b0ad9cb5cde8311734d1aa2
Jamie Davella <jamie.davella@gmail.com>
2016-05-15 15:17:35 -0400

    add pseudo code and loop for seeing if two objects have same key-value pair in release1

--- js/algos.js
+++ js/algos.js
@@ -33,3 +33,28 @@ console.log(longestStr(greetings));
 
 var sing = ["doooooooooooe", "ray", "me", "fa", "so", "la", "ti", "doe"];
 console.log(longestStr(sing));
+
+// Pseudocode Release 1 Find a Key-Value Match
+
+// input - two objects with key-value pairs
+// example: {name: "Steven", age: 54} and {name: "Tamir", age: 54}
+
+// steps -
+// loop over each key value pair for object 1
+// 	see if that object1 key-value pair is included in object 2.
+// if true return true and break loop
+// else if false continue loop and return false
+// 
+
+// output - if there is at least one match return true, else return false
+// example: true
+
+	for (key in obj1){
+		if (obj2[key] === obj1[key]){
+			1 === 1;
+			break;
+		}
+		else {
+			1 != 1;
+		}
+	}
